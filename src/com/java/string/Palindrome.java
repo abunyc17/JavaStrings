116aa95359fd6b48899cb2965e4463b795096b44
Abu <adamuabu@ymail.com>
2021-09-17 12:54:03 +0100

    palindrome class

---
+++ src/com/java/string/Palindrome.java
@@ -0,0 +1,59 @@
+package com.java.string;
+
+/**
+ * @author Abu
+ */
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Palindrome are word, phrase, or sequence that reads the same backwards as
+		 * forwards, e.g madam, liril, civic
+		 */
+
+		String stringValue = "liril";
+		char[] x = stringValue.toCharArray();
+		int size = x.length;
+		char[] y = new char[size];
+		char[] z = new char[size];
+		int i = 0;
+		while (i != size) {
+			y[size - 1 - i] = x[i];
+			++i;
+		}
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println("");
+		System.out.println("**********************First Logic******************************");
+		i = 0;
+		while (i != size) {
+			if(x[i] != y[i]) {
+				System.out.println("Not a palindrome!!!");
+				System.exit(0);
+			}else {
+				i++;
+				continue;
+			}
+		}
+		System.out.println("It is a palindrome!!!");
+		
+		//############################################################################################
+		
+		//Note: Once it is not a palindrome the control won't get here because of the system.exit(0) command.
+		
+		System.out.println("");
+		System.out.println("**********************Second Logic*********************************");
+		
+		String charToString = String.copyValueOf(y);
+
+		if (stringValue.equals(charToString)) {
+			System.out.println("this is a palindrome from second logic!");
+		} else {
+			System.out.println("Not a palindrome");
+		}
+
+	}
+
+}
