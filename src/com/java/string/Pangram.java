6005c6786b9446c3f22c9e6d4d751415f3958f45
Abu <adamuabu@ymail.com>
2021-10-04 18:36:27 +0100

    pangram

--- src/com/java/string/Pangram.java
+++ src/com/java/string/Pangram.java
@@ -11,10 +11,37 @@ public class Pangram {
 		/*
 		 * A pangram is a unique sentence in which every letter of the alphabet is used
 		 * at least once. The name comes from the Greek root words pan, meaning “all,”
-		 * E.g - A quick brown fox jumps over a lazy dog
+		 * E.g - The quick brown fox jumps over a lazy dog
 		 */
 		
+		String x = "THE QUICK BROWN FOX JUMPS OVER A LAZY DOG";
+		x = x.replace(" ", "");
+		char[] y = x.toCharArray();
+		int size = y.length;
 		
+		char[] a = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+		
+		int i = 0;
+		while(i != size) {
+			
+			int index = y[i] - 65;
+			a[index] = 1;
+			++i;
+		}
+		
+		i = 0;
+		while(i != 26) {
+			if(a[i] == 1) {
+				++i;
+			}else {
+				System.out.println("Not a pangram");
+				System.exit(0);
+			}
+			
+		}
+		
+		System.out.println("It's a pangram");
+				
 		
 	}
 
