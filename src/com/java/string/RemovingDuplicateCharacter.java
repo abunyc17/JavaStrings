16ce0304845bb0b27d9b9e45595056002feea1c9
Abu <adamuabu@ymail.com>
2021-10-14 18:38:59 +0100

    Removing duplicate characters

---
+++ src/com/java/string/RemovingDuplicateCharacter.java
@@ -0,0 +1,24 @@
+package com.java.string;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author Abu
+ */
+
+public class RemovingDuplicateCharacter {
+	
+	public static void main(String[] args) {
+		
+		String x = "SILLYSPIDERS";
+		char[] y = x.toCharArray();
+		int size = y.length;
+		
+		Map<Character, Integer> map = new LinkedHashMap<>();
+		
+	}
+
+}
+
+
